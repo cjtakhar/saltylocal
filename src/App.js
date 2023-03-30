9ccd991f15337dfe215fda1c0accb54171cbe096
cjtakhar <cjtakhar@gmail.com>
2023-03-29 19:52:03 -0700

    mod ep

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/saltylocal" element={<Home />} />
         <Route path="about" element={<About />} />
         <Route path="dashboard" element={<Dashboard />} />
       </Routes>
