1d82d298db46957a4b92fe08376c0154a44040ab
cjtakhar <cjtakhar@gmail.com>
2023-03-29 19:04:28 -0700

    rm import

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
+import { Navbar, Nav } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function Navigation() {
