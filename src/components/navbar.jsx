13aea07c483b31ab89f04ca4016bdf847ead0913
cjtakhar <cjtakhar@gmail.com>
2023-03-29 19:01:24 -0700

    add styles

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -13,6 +13,7 @@ export default function Navigation() {
         <Nav.Link className="nav-link" href="#contact">Events</Nav.Link>
         <Nav.Link className="nav-link" href="#contact">Shop</Nav.Link>
         <Nav.Link className="nav-link" href="#contact">Extras</Nav.Link>
+        <Nav.Link className="nav-link" href="#contact">About</Nav.Link>
       </Nav>
       <Nav className="ml-auto">
         <Nav.Link className="nav-link-login" href="#login">Login</Nav.Link>
