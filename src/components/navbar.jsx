0e281ff5f813bbc72584b28f20f06c14c76d245c
cjtakhar <cjtakhar@gmail.com>
2023-04-21 20:10:49 -0700

    update 2.0.2

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -5,20 +5,45 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 export default function Navigation() {
   return (
     <Navbar className="nb" expand="lg">
-    <Navbar.Brand className="navbrand" href="/saltylocal">Salty Local</Navbar.Brand>
-    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-    <Navbar.Collapse id="basic-navbar-nav">
-      <Nav className="mr-auto">
-        <Nav.Link className="nav-link-white" href="/news">News</Nav.Link>
-        <Nav.Link className="nav-link-white" href="/events">Events</Nav.Link>
-        <Nav.Link className="nav-link" href="/shop">Shop</Nav.Link>
-        <Nav.Link className="nav-link" href="/extras">Extras</Nav.Link>
-        <Nav.Link className="nav-link" href="/about">About</Nav.Link>
-      </Nav>
-      <Nav className="ml-auto">
-        <Nav.Link className="nav-link-login" href="/login">Login</Nav.Link>
-      </Nav>
-    </Navbar.Collapse>
-  </Navbar>
+      <Navbar.Brand className="navbrand" href="/saltylocal">
+        Salty Local
+      </Navbar.Brand>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon">
+          <i className="fas fa-bars"></i>
+        </span>
+      </button>
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link className="nav-link" href="/news">
+            News
+          </Nav.Link>
+          <Nav.Link className="nav-link" href="/events">
+            Events
+          </Nav.Link>
+          <Nav.Link className="nav-link" href="/shop">
+            Shop
+          </Nav.Link>
+          <Nav.Link className="nav-link" href="/extras">
+            Extras
+          </Nav.Link>
+          <Nav.Link className="nav-link" href="/about">
+            About
+          </Nav.Link>
+          <Nav.Link className="nav-link" href="/login">
+            Login
+          </Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
   );
 }
+
