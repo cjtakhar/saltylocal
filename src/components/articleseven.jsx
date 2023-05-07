6e12632086b3585be2b2548b221ef46fdd775042
cjtakhar <cjtakhar@gmail.com>
2023-05-06 21:25:52 -0700

    update

---
+++ src/components/articleseven.jsx
@@ -0,0 +1,16 @@
+import BayBridgeImage from '../images/baybridge.jpg';
+
+export default function ArticleSeven() {
+    return (
+        <div className="container article-container">
+        <div className="articleOne">
+            <img className="articleImg" src={BayBridgeImage} alt="bay bridge at golden hour from the city" />
+            <div className="articleText">
+                <h1 className="articleTitle">This is a Title for a Story</h1>
+                <p className="articleIntro">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ornare massa eget egestas purus viverra accumsan in. Faucibus et molestie ac feugiat sed lectus vestibulum mattis ullamcorper. Maecenas sed enim ut sem. Pulvinar neque laoreet suspendisse interdum consectetur libero id faucibus nisl. Dictum fusce ut placerat orci nulla pellentesque dignissim enim sit. Convallis posuere morbi leo urna molestie at. In vitae turpis massa sed elementum tempus. Ultrices gravida dictum fusce ut placerat orci nulla. Nisl purus in mollis nunc sed id. Cras pulvinar mattis nunc sed blandit libero. Ultricies leo integer malesuada nunc vel risus commodo.</p>
+                <p className="articleIntro">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ornare massa eget egestas purus viverra accumsan in. Faucibus et molestie ac feugiat sed lectus vestibulum mattis ullamcorper. Maecenas sed enim ut sem. Pulvinar neque laoreet suspendisse interdum consectetur libero id faucibus nisl. Dictum fusce ut placerat orci nulla pellentesque dignissim enim sit. Convallis posuere morbi leo urna molestie at. In vitae turpis massa sed elementum tempus. Ultrices gravida dictum fusce ut placerat orci nulla. Nisl purus in mollis nunc sed id. Cras pulvinar mattis nunc sed blandit libero. Ultricies leo integer malesuada nunc vel risus commodo.</p>
+            </div>
+         </div>
+         </div>
+    );
+}
--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,9 +1,21 @@
 import ArticleOne from "./articleone";
+import ArticleTwo from "./articletwo";
+import ArticleThree from "./articlethree";
+import ArticleFour from "./articlefour";
+import ArticleFive from "./articlefive";
+import ArticleSix from "./articlesix";
+import ArticleSeven from "./articleseven";
 
 export default function Home() {
     return (
         <div>  
             < ArticleOne />
+            < ArticleTwo />
+            < ArticleThree />
+            < ArticleFour />
+            < ArticleFive />
+            < ArticleSix />
+            < ArticleSeven />
         </div>
     );
 }
